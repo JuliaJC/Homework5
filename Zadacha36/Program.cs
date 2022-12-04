db7653b6804a098cb79d0d90cdd5b56efb865221
Julia <julso@LAPTOP-J5CQF40T.(none)>
2022-12-04 11:24:03 +0300

    Zadacha36 added

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int size = ReadInt("Введите размер массива: ");
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers, -99, 99);
+WriteArray(numbers);
+
+int sum = 0;
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(i % 2 != 0)
+    {
+        sum += numbers[i];
+    }
+    
+}
+
+Console.Write(sum);
+
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArrayRandomNumbers(int[] array, int min, int max)
+{
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = new Random().Next(min, max + 1);
+}
+}
+
+void WriteArray(int[] array)
+{
+for(int i = 0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+}
